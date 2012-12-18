998020b699ec15f7cab0e1b90e64ae42d04a1412
daniel <daniel@freshmilk.tv>
2012-12-18 10:48:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     author=u'Daniel Brüggemann',
     author_email='daniel@freshmilk.tv',
     url='http://www.freshmilk.tv',
-    description="""Freshmilk article App""",
+    description="""Freshmilk article app""",
     packages=find_packages(),
     namespace_packages=[],
     include_package_data=True,
     zip_safe=False,
     license='WTFPL2',
-    install_requires=['django-tinymce', 'fm.portlet', '']
+    install_requires=['django-ckeditor', 'fm.utils', 'fm.portlet']
 )
